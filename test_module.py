1a3a8c15116e426cabf97f331a6656297533f0d1
Brian <bstanton773@gmail.com>
2023-07-25 21:22:52 -0500

    forgot to add

---
+++ test_module.py
@@ -0,0 +1,8 @@
+def greet(first_name):
+    print(f"Hello {first_name.title()}, how are you doing?")
+
+def leave(first_name):
+    print(f"Goodbye {first_name.title()}, it was a pleasure seeing you.")
+
+
+print('Hello this is the test module file being run!')
